178b5bc6bd52ed22e1928ad2ed5568c7edffc31f
unknown <fernandoselvino@gmail.com>
2020-04-10 19:44:00 -0300

    desafio conceitos de node

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  //uuid() - gera um id unico
+  const repository = { id: uuid(), title, url, techs, likes: 0, }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: 'Repositorio não existe' })
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes //recebe o valor de likes automaticamente
+  }
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  //Realiza busca pelo id vindo dos parametros
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  //se retornar  diferente de -1 é porque o repositorio existe
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex, 1)
+    //senão retorna erro
+  } else {
+    return response.status(400).json({ error: 'Repositorio não existe' })
+  }
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: 'Repositorio não existe' })
+  }
+
+  //inclementa o campo likes a cada like recebido
+  repositories[repositoryIndex].likes++
+
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
